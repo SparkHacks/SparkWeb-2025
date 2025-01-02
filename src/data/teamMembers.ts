10f63ca7e1c55f981ce10a13732d398b1142e5c5
Mightymanh <dphan8@uic.edu>
2025-01-02 12:06:05 -0600

    hovering and linkedin to meet the team

--- src/data/teamMembers.ts
+++ src/data/teamMembers.ts
@@ -1,152 +1,152 @@
-export const teamMembers = [ 
+export const teamMembers = [
   {
-    name: "Aarav",
-    title: "",
+    name: "Jelena Gvero",
+    title: "Director",
+    image: "/team/jelena.JPG",
+    linkedin: "https://www.linkedin.com/in/jelena-gvero/"
+  },
+  {
+    name: "Yamaan Nandolia",
+    title: "Director",
+    image: "/team/yamaan.JPG",
+    linkedin: "https://www.linkedin.com/in/yamaan-nandolia/"
+  },
+  {
+    name: "Aarav Surkatha",
+    title: "Logistics - Team Lead",
     image: "/team/aarav.JPG",
-    linkedin: ""
-  },
-  {
-    name: "Adrian Knight",
-    title: "",
-    image: "/team/adrian.JPG",
-    linkedin: ""
-  },
-  {
-    name: "Amy",
-    title: "",
-    image: "/team/amy.JPG",
-    linkedin: ""
+    linkedin: "https://www.linkedin.com/in/aarav-surkatha/"
   },
   {
     name: "Brenda Leyva",
-    title: "",
+    title: "Outreach - Team Lead",
     image: "/team/brenda.JPG",
-    linkedin: ""
+    linkedin: "https://www.linkedin.com/in/brleyva/"
   },
   {
-    name: "Daniel",
-    title: "",
-    image: "/team/danny.JPG",
-    linkedin: ""
-  },
-  {
-    name: "Fey",
-    title: "",
+    name: "Fey Ogutuga",
+    title: "Outreach - Team Lead",
     image: "/team/fey.JPG",
-    linkedin: ""
+    linkedin: "https://www.linkedin.com/in/fey-ogutuga-8b89a0216/"
   },
   {
-    name: "Flori Che",
-    title: "",
-    image: "/team/flori.JPG",
-    linkedin: ""
+    name: "Daniel Mroz",
+    title: "Communications - Team Lead",
+    image: "/team/danny.JPG",
+    linkedin: "https://www.linkedin.com/in/mroz-daniel/"
   },
   {
-    name: "Gerard",
-    title: "",
-    image: "/team/gerard.JPG",
-    linkedin: ""
+    name: "Yasmin Sawaf",
+    title: "Experience - Team Lead",
+    image: "/team/yasmin.JPG",
+    linkedin: "https://www.linkedin.com/in/yasmin-sawaf/"
   },
   {
-    name: "Hamza",
-    title: "",
+    name: "Naga Maddipudi",
+    title: "Experience - Team Lead",
+    image: "/team/naga.JPG",
+    linkedin: "https://www.linkedin.com/in/naga-maddipudi/"
+  },
+  {
+    name: "Kevin Cordero",
+    title: "Web Development - Team Lead",
+    image: "/team/kevin.JPG",
+    linkedin: "https://www.linkedin.com/in/kevin-cordero/"
+  },
+  {
+    name: "Hamza Gaziuddin",
+    title: "Web Development - Team Lead",
     image: "/team/hamza.JPG",
-    linkedin: ""
+    linkedin: "https://www.linkedin.com/in/hamza-gaziuddin/"
   },
   {
-    name: "Jelena Gvero",
-    title: "",
-    image: "/team/jelena.JPG",
-    linkedin: ""
+    name: "Amy Kodama",
+    title: "Outreach",
+    image: "/team/amy.JPG",
+    linkedin: "https://www.linkedin.com/in/amykodama/"
+  },
+  {
+    name: "Khin Yuupar Myat",
+    title: "Outreach",
+    image: "/team/yuupar.JPG",
+    linkedin: "https://www.linkedin.com/in/khinyuuparmyat/"
+  },
+  {
+    name: "Niharika Patil",
+    title: "Outreach",
+    image: "/team/niharika.JPG",
+    linkedin: "https://www.linkedin.com/in/niharika-patil1468/"
+  },
+  {
+    name: "Yacine Niasse",
+    title: "Outreach",
+    image: "/team/yacine.JPG",
+    linkedin: "https://www.linkedin.com/in/yacineniasse/"
+  },
+  {
+    name: "Gerard Wilbert",
+    title: "Logistics",
+    image: "/team/gerard.JPG",
+    linkedin: "https://www.linkedin.com/in/gerard-wilbert-a94636291/"
+  },
+  {
+    name: "Karina Latasiewicz",
+    title: "Logistics",
+    image: "/team/karina.JPG",
+    linkedin: "https://www.linkedin.com/in/karina-latasiewicz/"
+  },
+  {
+    name: "Nathan Trinh",
+    title: "Logistics",
+    image: "/team/nathan.JPG",
+    linkedin: "https://www.linkedin.com/in/khoi-nguyen-trinh/"
+  },
+  {
+    name: "Kaustubha Medikundam",
+    title: "Communications",
+    image: "/team/kau.JPG",
+    linkedin: "https://www.linkedin.com/in/kmedikundam/"
+  },
+  {
+    name: "Lakshmi Krishnan",
+    title: "Communications",
+    image: "/team/lakshmi.JPG",
+    linkedin: "https://www.linkedin.com/in/lkris2/"
   },
   {
     name: "Kaito Sekiya",
     title: "Experience",
     image: "/team/kaito.JPG",
-    linkedin: ""
+    linkedin: "https://www.linkedin.com/in/kaitosekiya/"
   },
   {
-    name: "",
-    title: "",
-    image: "/team/karina.JPG",
-    linkedin: ""
-  },
-  {
-    name: "Kaustubha Medikundam",
-    title: "",
-    image: "/team/kau.JPG",
-    linkedin: ""
-  },
-  {
-    name: "Kevin Cordero",
-    title: "",
-    image: "/team/kevin.JPG",
-    linkedin: ""
-  },
-  {
-    name: "Krisha",
-    title: "",
+    name: "Krisha Patel",
+    title: "Experience",
     image: "/team/krisha.JPG",
-    linkedin: ""
+    linkedin: "https://www.linkedin.com/in/krisha-patel-61b458220/"
   },
   {
-    name: "Lakshmi",
-    title: "",
-    image: "/team/lakshmi.JPG",
-    linkedin: ""
+    name: "Rahin Jain",
+    title: "Experience",
+    image: "/team/rahin.jpeg",
+    linkedin: "https://www.linkedin.com/in/rahinjain/"
+  },
+  {
+    name: "Adrian Knight",
+    title: "Web Development",
+    image: "/team/adrian.JPG",
+    linkedin: "https://www.linkedin.com/in/adrian-o-knight/"
+  },
+  {
+    name: "Florianne Che",
+    title: "Web Development",
+    image: "/team/flori.JPG",
+    linkedin: "https://www.linkedin.com/in/florianne-che/"
   },
   {
     name: "Manh Phan",
-    title: "",
+    title: "Web Development",
     image: "/team/manh.JPG",
-    linkedin: ""
-  },
-  {
-    name: "Naga",
-    title: "",
-    image: "/team/naga.JPG",
-    linkedin: ""
-  },
-  {
-    name: "Nathan Trinh",
-    title: "",
-    image: "/team/nathan.JPG",
-    linkedin: ""
-  },
-  {
-    name: "Niharika Patil",
-    title: "",
-    image: "/team/niharika.JPG",
-    linkedin: ""
-  },
-  {
-    name: "Rahin",
-    title: "",
-    image: "/team/rahin.jpeg",
-    linkedin: ""
-  },
-  {
-    name: "Yacine",
-    title: "",
-    image: "/team/yacine.JPG",
-    linkedin: ""
-  },
-  {
-    name: "Yamaan Nandolia",
-    title: "",
-    image: "/team/yamaan.JPG",
-    linkedin: ""
-  },
-  {
-    name: "Yasmin",
-    title: "",
-    image: "/team/yasmin.JPG",
-    linkedin: ""
-  },
-  {
-    name: "Yuupar",
-    title: "",
-    image: "/team/yuupar.JPG",
-    linkedin: ""
+    linkedin: "https://www.linkedin.com/in/manhphan1801/"
   },
 ]
